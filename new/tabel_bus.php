058cfb86cb4506a8fe66a43086cf725e85352eef
rayhan <rayhanzz772@gmail.com>
2024-05-01 16:06:06 +0700

    test

--- new/tabel_bus.php
+++ new/tabel_bus.php
@@ -47,6 +47,7 @@
 
     <!-- Icons. Uncomment required icon fonts -->
     <link rel="stylesheet" href="../assets/vendor/fonts/boxicons.css" />
+    <link rel="stylesheet" href="style.css">
 
     <!-- Core CSS -->
     <link rel="stylesheet" href="../assets/vendor/css/core.css" class="template-customizer-core-css" />
@@ -64,6 +65,9 @@
     <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
     <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
     <script src="../assets/js/config.js"></script>
+    <style>
+      
+    </style>
 </head>
 
 <body>
@@ -150,65 +154,46 @@
                         <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Bus /</span> Basic Bus</h4>
 
                         <!-- Basic Bootstrap Table -->
-                        <div class="card">
-                            <h5 class="card-header">List Bus</h5>
-                            <div class="table-responsive text-nowrap">
-                                <table class="table">
-                                    <thead>
-                                        <tr>
-                                        <th scope="col">No</th>
-                                        <th scope="col">Nama Bus</th>
-                                        <th scope="col">ID Bus</th>
-                                        <th scope="col">Tujuan Bus</th>
-                                        <th scope="col">Harga</th>
-                                        <th scope="col">Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody class="table-border-bottom-0">
-                                        
-                                        <?php
-                                            $no = 1;
-                                            foreach ($db->tampil_data_bus() as $x) {
-                                                ?>
-                                                <tr>
-                                                    <td><?php echo $no++; ?></td>
-                                                    <td><?php echo $x['nama_bus']; ?></td>
-                                                    <td><?php echo $x['id_bus']; ?></td>
-                                                    <td><?php echo $x['kota']; ?></td>
-                                                    <td><?php echo $x['harga_bus']; ?></td>
-                                                    <td>
-                                                    <a href="#" class="btn btn-sm btn-info" data-toggle="modal" data-target="#insertDataModal<?php echo $x['id_bus']; ?>">Beli</a>
-
-                                                    <!-- <button type="button" class='btn btn-primary' data-toggle="modal" data-target="#insertDataModal">Beli</button> -->
-                                                  
-                                                  </td>
-
-                                                  <?php  
-
-        include "koneksi.php";
-        $result = mysqli_query($koneksi, "select * from nama_bus");
 
 
-        $id = $x['id_bus'];
 
-        $sql = mysqli_query($koneksi, "SELECT * FROM nama_bus 
-        INNER JOIN harga_bus ON nama_bus.id_bus = harga_bus.id_bus 
-        INNER JOIN id_kota ON nama_bus.id_kota = id_kota.id_kota 
-        INNER JOIN id_bus ON nama_bus.id_foto = id_bus.id_foto 
-        WHERE nama_bus.id_bus = '$id'");
+<!-- table disini -->
 
-        $tampil = mysqli_fetch_array($sql);
+<?php foreach ($db->tampil_data_bus() as $x): ?>
+<div class="card">
+  <div class="card-body">
+    <div class="text-section">
+    <h5><?php echo $x['nama_bus']; ?> (<?php echo $x['id_bus']; ?>)</h5>
+    <p class="card-text">Tujuan Bus: <?php echo $x['kota']; ?></p>
+    </div>
+    <div class="cta-section">
+      <div>
+        <p class="card-text"><?php echo $x['harga_bus']; ?></p>
+      </div>
+      <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#insertDataModal<?php echo $x['id_bus']; ?>">Beli</a>
+    </div>
+  </div>
+</div>  
 
+    <!-- Modal -->
+    <?php  
+include "koneksi.php";
+$result = mysqli_query($koneksi, "select * from nama_bus");
 
-        ?>
-  
+$id = $x['id_bus'];
 
-                                                  
+$sql = mysqli_query($koneksi, "SELECT * FROM nama_bus 
+INNER JOIN harga_bus ON nama_bus.id_bus = harga_bus.id_bus 
+INNER JOIN id_kota ON nama_bus.id_kota = id_kota.id_kota 
+INNER JOIN id_bus ON nama_bus.id_foto = id_bus.id_foto 
+WHERE nama_bus.id_bus = '$id'");
 
-                                                  <div class="modal" id="insertDataModal<?php echo $x['id_bus']; ?>" tabindex="-1" role="dialog" aria-labelledby="insertDataModalLabel" aria-hidden="true">
-  
-  
-                                                    <div class="modal-dialog" role="document">
+$tampil = mysqli_fetch_array($sql);
+
+?>
+
+<div class="modal" id="insertDataModal<?php echo $x['id_bus']; ?>" tabindex="-1" role="dialog" aria-labelledby="insertDataModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="insertDataModalLabel">Tambah Data Produk</h5>
@@ -218,12 +203,11 @@
       </div>
       <div class="modal-body">
         <form action="beli.php" method="POST" enctype="multipart/form-data">
-        <input type="hidden" name="id" value="<?php echo $x['id_bus']; ?>">
+          <input type="hidden" name="id" value="<?php echo $x['id_bus']; ?>">
           <div class="form-group">
             <label for="exampleInputEmail1" class="form-label">Nama Lengkap :</label>
             <input name="nama" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
           </div>
-
           <div class="form-group">
             <label for="jenis_kelamin">Jenis Kelamin:</label>
             <select name="jenis_kelamin" class="form-control">
@@ -235,64 +219,44 @@
               ?>
             </select>
           </div>
-
-
           <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Nomor identitas</label>
             <input name="nomor_identitas" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
           </div>
-
           <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Nomor Handphone</label>
             <input name="no" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
           </div>
-          
-
-
-                <input type="hidden" name="nama_po" value="<?php echo $tampil['nama_bus'] ?>">
-           
-
-            <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">Tanggal Berangkat</label>
-              <input name="tgl" type="date" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            </div>
-
-
-                
-                <input type="hidden" name="tujuan" value="<?php echo $tampil['kota']; ?>">
-                
-           
-
-
-                
-                    <input type="hidden" name="biaya_tiket" value="<?php echo $tampil['harga_bus']; ?>">
-
-                    <div class="form-group">
-                        <label for="qty">Jumlah Tiket Dewasa</label>
-                        <select name="qty" class="form-control">
-                            <option value="">- Jumlah -</option>
-                            <?php
-                            for ($x = 1; $x <= 10; $x++) {
-                                echo '<option value="' . $x . '">' . $x . '</option>';
-                            }
-                            ?>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="qty_bayi">Jumlah Tiket Anak</label>
-                        <select name="qty_bayi" class="form-control">
-                            <option value="">- Jumlah -</option>
-                            <option value="0">0</option>
-                            <?php
-                            for ($x = 1; $x <= 10; $x++) {
-                                echo '<option value="' . $x . '">' . $x . '</option>';
-                            }
-                            ?>
-                        </select>
-                    </div>
-                
-           
-
+          <input type="hidden" name="nama_po" value="<?php echo $tampil['nama_bus'] ?>">
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Tanggal Berangkat</label>
+            <input name="tgl" type="date" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+          </div>
+          <input type="hidden" name="tujuan" value="<?php echo $tampil['kota']; ?>">
+          <input type="hidden" name="biaya_tiket" value="<?php echo $tampil['harga_bus']; ?>">
+          <div class="form-group">
+            <label for="qty">Jumlah Tiket Dewasa</label>
+            <select name="qty" class="form-control">
+              <option value="">- Jumlah -</option>
+              <?php
+              for ($x = 1; $x <= 10; $x++) {
+                echo '<option value="' . $x . '">' . $x . '</option>';
+              }
+              ?>
+            </select>
+          </div>
+          <div class="form-group">
+            <label for="qty_bayi">Jumlah Tiket Anak</label>
+            <select name="qty_bayi" class="form-control">
+              <option value="">- Jumlah -</option>
+              <option value="0">0</option>
+              <?php
+              for ($x = 1; $x <= 10; $x++) {
+                echo '<option value="' . $x . '">' . $x . '</option>';
+              }
+              ?>
+            </select>
+          </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
             <button type="submit" name="hitung" class="btn btn-primary">Submit</button>
@@ -303,52 +267,18 @@
   </div>
 </div>
 
-                                                </tr>
-                                            <?php 
-                                            } 
-                                            ?>     
-                                            <tr>
-                                            <div class="row">
-                                            </div>
+  <?php endforeach; ?>
+  
+</div>
 
-                                            </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                 </div>
+
+<!-- end table -->
+
+
               <!--/ Basic Bootstrap Table -->
 
             </div>
             <!-- / Content -->
-
-            <!-- Footer -->
-            <footer class="content-footer footer bg-footer-theme">
-              <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
-                <div class="mb-2 mb-md-0">
-                  ©
-                  <script>
-                    document.write(new Date().getFullYear());
-                  </script>
-                  , made with ❤️ by
-                  <a href="https://themeselection.com" target="_blank" class="footer-link fw-bolder">ThemeSelection</a>
-                                                    </div>
-                                                    <div>
-                                                        <a href="https://themeselection.com/license/" class="footer-link me-4" target="_blank">License</a>
-                                                        <a href="https://themeselection.com/" target="_blank" class="footer-link me-4">More Themes</a>
-
-                                                        <a href="https://themeselection.com/demo/sneat-bootstrap-html-admin-template/documentation/" target="_blank" class="footer-link me-4">Documentation</a
-                  >
-
-                  <a
-                    href="https://github.com/themeselection/sneat-html-admin-template-free/issues"
-                    target="_blank"
-                    class="footer-link me-4"
-                    >Support</a
-                  >
-                </div>
-              </div>
-            </footer>
-            <!-- / Footer -->
 
             <div class="content-backdrop fade"></div>
           </div>
